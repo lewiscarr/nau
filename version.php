d1e50bf85bdc514e59a97b24e9e271e7e44830fa
Majid Hussain <majid@majidhussain.co.uk>
2016-08-30 11:33:24 +0100

    #1554 - Adjust course headers to include NAU tear graphic and other course header changes.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016072017;
+$plugin->version   = 2016083000;
 $plugin->requires  = 2014050800;
 $plugin->component = 'theme_nau';
 $plugin->release = '1.3.1';
