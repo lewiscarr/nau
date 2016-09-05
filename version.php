c293e2170c1702cc8da9ce513ad78aa2ea9b19f4
Lewis Carr <lewis.carr@gmail.com>
2016-09-05 18:02:46 +0100

    Majid Rollback + Lewis Brushstroke

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016072018;
+$plugin->version   = 2016090500;
 $plugin->requires  = 2014050800;
 $plugin->component = 'theme_nau';
 $plugin->release = '1.3.1';
