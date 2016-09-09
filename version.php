593c169fe686a25c0fc80f1401835dd3516c15ba
Majid Hussain <majid.hussain@tituslearning.com>
2016-09-09 17:29:53 +0100

    #1653 / #1654 - Fix grid headers so they no longer overlap and hide blocks column when grading assignments

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016090600;
+$plugin->version   = 2016090900;
 $plugin->requires  = 2014050800;
 $plugin->component = 'theme_nau';
 $plugin->release = '1.3.1';
