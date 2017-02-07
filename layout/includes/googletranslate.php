21622ff813412b27d100383c766c1c0bc01aa8c0
Steve A <dev@anatai.com>
2017-02-07 15:08:00 +0000

    Added back the translation_preference value

--- layout/includes/googletranslate.php
+++ layout/includes/googletranslate.php
@@ -1,3 +1,4 @@
+<?php $translate_preference = get_user_preferences('theme_nau_googletranslate_on', 0, $USER->id); ?>
 <div id="google_translate_element"<?php if ($translate_preference) echo ' style="display: none";' ?>></div>
 <script type="text/javascript">
 function googleTranslateElementInit() {
